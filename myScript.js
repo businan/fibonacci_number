6c3ad82435b8860b4f73b86d0352afe0a730cb45
businan <yorulmazburaksinan@gmail.com>
2020-09-23 08:52:47 +0200

    finished to project fibonacci

--- myScript.js
+++ myScript.js
@@ -1,20 +1,29 @@
-function check(){
+function check() {
     const number = document.getElementById("fibonacci_number").value;
-
-    let fibonacciArray = []
-    for(let i=0; i<parseInt(number)+2; i++){
-        if (fibonacciArray.length === 0){
-            //console.log('array bos')
-            fibonacciArray[i]=0;
-            //console.log("fibanacci aray 0  "+ fibonacciArray )
-        }else if(fibonacciArray.length === 1){
-            fibonacciArray[i]=1;
-            //console.log("fibanacci aray 1  "+ fibonacciArray )
-        }else{
-            fibonacciArray[i] = fibonacciArray[i-1]+fibonacciArray[i-2];
-            //console.log("fibanacci aray  "+ fibonacciArray )
+    console.log(Number.isInteger(+number))
+    if (number !== "") {
+        let fibonacciArray = []
+        for (let i = 0; i < parseInt(number) + 2; i++) {
+            if (fibonacciArray.length === 0) {
+                //console.log('array bos')
+                fibonacciArray[i] = 0;
+                //console.log("fibanacci aray 0  "+ fibonacciArray )
+            } else if (fibonacciArray.length === 1) {
+                fibonacciArray[i] = 1;
+                //console.log("fibanacci aray 1  "+ fibonacciArray )
+            } else {
+                fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+                //console.log("fibanacci aray  "+ fibonacciArray )
+            }
         }
+        console.log(fibonacciArray[fibonacciArray.length - 1])
+        let result = fibonacciArray[fibonacciArray.length - 1];
+        document.getElementById('result').innerHTML = "The " + number + " th fibonacci number is " + result;
+    } else {
+        document.getElementById("result").innerHTML = "Input field must be Entered...";
     }
-    console.log(fibonacciArray[fibonacciArray.length-1])
-    document.getElementById('result').innerHTML = fibonacciArray[fibonacciArray.length-1];
+
+}
+function reset(){
+    document.getElementById('fibonacci_number').value = "";
 }
